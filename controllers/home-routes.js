f0693ca6fe94d0ac7bdd965e482de224a27ae2f2
Kate Spitalnic <kespitalnic@gmail.com>
2022-03-03 20:45:17 -0500

    login into home routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,7 +2,14 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Car } = require('../models');
 
-
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+      res.redirect('/');
+      return;
+    }
+  
+    res.render('login');
+  });
 
 
 module.exports = router;
