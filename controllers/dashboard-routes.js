df49f7328d1fcdbb95a0b26c72cdb5f68d4d1732
James Barry <jbm03003@gmail.com>
2022-03-05 12:06:49 -0500

    added edit/update to dashboard-routes

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -44,4 +44,31 @@ router.get('/', withAuth, (req, res) => {
       });
   });
 
+  // NEED TO FINISH
+  router.put('edit/:id', withAuth, (req, res) => {
+    Review.update({
+        dents: req.body.dents,
+        scratches: req.body.scratches,
+        odor: req.body.odor,
+        stains: req.body.stains,
+        overall_rating: req.body.overall_rating,
+        review: req.body.review,        
+    },
+    {
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbReviewData => {
+        if(!dbReviewData) {
+            res.status(404).json({ message: 'No review found!'});
+            return;
+        }        
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
   module.exports = router;
